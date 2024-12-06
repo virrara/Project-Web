8f6e915f21499a5efbeec27448e6f83e5fea5ff6
virrara <110639874+virrara@users.noreply.github.com>
2024-12-06 08:31:41 +0700

    Adding files (1)...

--- Pemrograman Web - Zakat Fitrah/html/autofill.php
+++
@@ -1,9 +0,0 @@
-<?php
-include 'config.php';
-$kategori = $_GET['kategori'];
-$query = mysqli_query($conn, "select * from kategori_mustahik where nama_kategori='$kategori'");
-$kategori_mustahik = mysqli_fetch_array($query);
-$data = array(
-            'hal'      =>  $kategori_mustahik['hak'],);
- echo json_encode($data);
-?>
