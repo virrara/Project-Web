e3c9f4bc78cbdab8adac71bc19473c357fd7feac
virrara <110639874+virrara@users.noreply.github.com>
2024-12-06 08:28:19 +0700

    Adding files (1)...

---
+++ Pemrograman Web - Input Information/assets/js/main.js
@@ -0,0 +1,61 @@
+//WindowsPrompt
+function mssg(){
+    alert("Please select the 'Create Data' menu, then fill in your information then the results will come out");
+}
+
+//InputData
+function inputdata(){
+    var npm = prompt('Input your NPM')
+    var name = prompt('What is your name?')
+    var address = prompt('Where`s your address?')
+    var confirm = window.confirm("Are you sure the data is correct?")
+
+    var x = document.getElementById("npm");    
+    x.innerHTML = npm;
+    var y = document.getElementById("name");    
+    y.innerHTML = name;
+    var z = document.getElementById("address");    
+    z.innerHTML = address;
+}
+
+/*===== MENU SHOW Y HIDDEN =====*/
+const navMenu = document.getElementById('nav-menu'),
+    toggleMenu = document.getElementById('nav-toggle'),
+    closeMenu = document.getElementById('nav-close')
+
+// SHOW
+toggleMenu.addEventListener('click', ()=>{
+    navMenu.classList.toggle('show')
+})
+
+// HIDDEN
+closeMenu.addEventListener('click', ()=>{
+    navMenu.classList.remove('show')
+})
+
+/*===== MOUSEMOVE HOME IMG =====*/
+document.addEventListener('mousemove', move);
+function move(e){
+    this.querySelectorAll('.move').forEach(layer =>{
+        const speed = layer.getAttribute('data-speed')
+
+        const x = (window.innerWidth - e.pageX*speed)/120
+        const y = (window.innerHeight - e.pageY*speed)/120
+
+        layer.style.transform = `translateX(${x}px) translateY(${y}px)`
+    })
+}
+
+/*===== GSAP ANIMATION =====*/
+// NAV
+gsap.from('.nav__logo, .nav__toggle', {opacity: 0, duration: 1, delay:2, y: 10})
+gsap.from('.nav__item', {opacity: 0, duration: 1, delay: 2.1, y: 30, stagger: 0.2,})
+
+// HOME
+gsap.from('.home__title', {opacity: 0, duration: 1, delay:1.6, y: 30})
+gsap.from('.home__title1', {opacity: 0, duration: 1, delay:1.6, y: 30})
+gsap.from('.home__description', {opacity: 0, duration: 1, delay:1.8, y: 30})
+gsap.from('.home__button', {opacity: 0, duration: 1, delay:2.1, y: 30})
+gsap.from('.home__img', {opacity: 0, duration: 1, delay:1.3, y: 30})
+
+
